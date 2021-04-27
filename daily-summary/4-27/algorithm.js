e07033ca0a1408779bbb80a728bb5ca64341bdbe
Roadster <jsonkk@163.com>
2021-04-27 14:38:12 +0800

    feat: update 4-27 notes

--- daily-summary/4-27/algorithm.js
+++ daily-summary/4-27/algorithm.js
@@ -1,6 +1,7 @@
 /**
  * 判断一个链表是否为回文链表
  * palindrome: 回文
+ * leetcode: https://leetcode-cn.com/problems/palindrome-linked-list/
  */
 const mocks = {
   value: "a",
@@ -21,7 +22,8 @@ const mocks = {
 /**
  * 1. 链表转换数组
  * 2. 双指针判断是否回文
- * 时间复杂度: O(n), 
+ * 时间复杂度: O(n), 第一步, 将链表遍历赋值到数组中O(n); 第二步, 双指针判断执行了O(n/2),即O(n), 所以总复杂度O(2n) = o(n).
+ * 空间复杂度: O(n), n代表链表的元素个数,我们只使用了一个数组来代表链表的个数.
  */
 const isPalindrome = (head) => {
   const arr = [];
@@ -42,6 +44,10 @@ const isPalindrome = (head) => {
 };
 /**
  * 快慢指针
+ * 思考: 通过正向和逆向的迭代匹配, 将链表前半部分reverse, 与后半部分进行匹配.
+ * 带来的问题: 修改了链表结构
+ * 时间复杂度:O(n), n指链表大小
+ * 空间复杂度O(1), 我们只会修改原本链表节点, 对战上不超过O(1)
  * slower: 指向中间节点
  * 通过slower截取链表前一半, 通过翻转跟后一段进行比较
  */
@@ -77,3 +83,4 @@ const isPalindrome = (head) => {
   }
   return true;
 };
+
